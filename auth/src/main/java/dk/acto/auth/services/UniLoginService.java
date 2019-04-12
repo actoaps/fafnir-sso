4f7b6254e1ab76bcb4723f115584104c368e234f
Jesper Preuss <jp@acto.dk>
2019-04-12 10:49:09 +0200

    Using spring boot templates as a thymeleaf Added models to thymeleaf

--- auth/src/main/java/dk/acto/auth/services/UniLoginService.java
+++ auth/src/main/java/dk/acto/auth/services/UniLoginService.java
@@ -1,18 +1,22 @@
 package dk.acto.auth.services;
 
 import dk.acto.auth.ActoConf;
+import dk.acto.auth.providers.UniLoginConstants;
 import dk.acto.auth.providers.UniLoginProvider;
 import dk.acto.auth.providers.validators.UniLoginValidator;
 import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+@Controller
 @Slf4j
 @RequestMapping("unilogin")
 public class UniLoginService implements Callback3Service {
@@ -27,30 +31,35 @@ public class UniLoginService implements Callback3Service {
 	
 	
 	@GetMapping
+	@ResponseBody
 	public void authenticate(HttpServletResponse response) {
 		Try.of(() -> ServiceHelper.functionalRedirectTo(response, provider::authenticate));
 	}
 	
 	@GetMapping("callback")
+	@ResponseBody
 	public void callback(HttpServletResponse response, @RequestParam String user, @RequestParam String timestamp, @RequestParam String auth) {
 		Try.of(() -> ServiceHelper.functionalRedirectTo(response, () -> provider.callback(user, timestamp, auth)));
 	}
 	
 	@GetMapping("org")
-	public String getOrg(@RequestParam String user, @RequestParam String timestamp, @RequestParam String auth, @RequestHeader("Accept-Language") String locale) {
+	public String getOrg(@RequestParam String user, @RequestParam String timestamp, @RequestParam String auth, @RequestHeader("Accept-Language") String locale, Model model) {
 		var institutionList = provider.getInstitutionList(user);
-		var model = Map.of(
-				"user", user,
-				"timestamp", timestamp,
-				"auth", auth,
-				"institutionList", institutionList
+		model.addAllAttributes(
+				new HashMap<String, Object>() {{
+					put(UniLoginConstants.USER_ID, user);
+					put(UniLoginConstants.TIMESTAMP, timestamp);
+					put(UniLoginConstants.STATE_AUTH_ENCODED, auth);
+					put("institutionList", institutionList);
+				}}
 		);
-		return "/thymeleaf/ChooseInstitutionUni-Login" +
+		return "thymeleaf/ChooseInstitutionUni-Login" +
 				ServiceHelper.getLocaleStr(locale, "da", "en") +
 				".thymeleaf";
 	}
 	
 	@PostMapping("org")
+	@ResponseBody
 	public void postOrg(HttpServletResponse response, @RequestParam String user, @RequestParam String timestamp, @RequestParam String auth, @RequestParam String institution) {
 		Try.of(() -> ServiceHelper.functionalRedirectTo(response, () -> provider.callbackWithInstitution(user, timestamp, auth, institution)));
 	}
