37cb8867b5df7b5e205d1f5223806119e3dc6b06
Jesper Preuss <jp@acto.dk>
2019-04-29 16:15:15 +0200

    Sonar qube removed Double Brace Initializer

--- auth/src/main/java/dk/acto/auth/services/UniLoginService.java
+++ auth/src/main/java/dk/acto/auth/services/UniLoginService.java
@@ -13,7 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @Slf4j
@@ -21,42 +21,42 @@ import java.util.HashMap;
 public class UniLoginService implements Callback3Service {
 	private final UniLoginProvider provider;
 	private final ActoConf actoConf;
-	
+
 	@Autowired
 	public UniLoginService(UniLoginProvider provider, @Validated(UniLoginValidator.class) ActoConf actoConf) {
 		this.provider = provider;
 		this.actoConf = actoConf;
 	}
-	
-	
+
+
 	@GetMapping
 	@ResponseBody
 	public void authenticate(HttpServletResponse response) {
 		Try.of(() -> ServiceHelper.functionalRedirectTo(response, provider::authenticate));
 	}
-	
+
 	@GetMapping("callback")
 	@ResponseBody
 	public void callback(HttpServletResponse response, @RequestParam String user, @RequestParam String timestamp, @RequestParam String auth) {
 		Try.of(() -> ServiceHelper.functionalRedirectTo(response, () -> provider.callback(user, timestamp, auth)));
 	}
-	
+
 	@GetMapping("org")
 	public String getOrg(@RequestParam String user, @RequestParam String timestamp, @RequestParam String auth, @RequestHeader("Accept-Language") String locale, Model model) {
 		var institutionList = provider.getInstitutionList(user);
 		model.addAllAttributes(
-				new HashMap<String, Object>() {{
-					put(UniLoginConstants.USER_ID, user);
-					put(UniLoginConstants.TIMESTAMP, timestamp);
-					put(UniLoginConstants.STATE_AUTH_ENCODED, auth);
-					put("institutionList", institutionList);
-				}}
+				Map.of(
+						UniLoginConstants.USER_ID, user,
+						UniLoginConstants.TIMESTAMP, timestamp,
+						UniLoginConstants.STATE_AUTH_ENCODED, auth,
+						"institutionList", institutionList
+				)
 		);
 		return "thymeleaf/ChooseInstitutionUni-Login" +
 				ServiceHelper.getLocaleStr(locale, "da", "en") +
 				".thymeleaf";
 	}
-	
+
 	@PostMapping("org")
 	@ResponseBody
 	public void postOrg(HttpServletResponse response, @RequestParam String user, @RequestParam String timestamp, @RequestParam String auth, @RequestParam String institution) {
