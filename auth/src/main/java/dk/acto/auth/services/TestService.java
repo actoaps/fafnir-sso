9f0c0f29699a28ea56ac210f1648118c41dbebc3
Elias Jørgensen <EliasJorgensen@users.noreply.github.com>
2019-08-23 15:23:30 +0200

    Register testMode endpoint

--- auth/src/main/java/dk/acto/auth/services/TestService.java
+++ auth/src/main/java/dk/acto/auth/services/TestService.java
@@ -7,6 +7,7 @@ import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +23,9 @@ public class TestService {
 	public TestService(TestProvider provider, @Validated(TestValidator.class) ActoConf actoConf) {
 		this.provider = provider;
 	}
-	
+
+	@GetMapping
 	public void authenticate(HttpServletResponse response) {
 		Try.of(() -> ServiceHelper.functionalRedirectTo(response, provider::authenticate));
 	}
-	
-	
 }
