a14df77be267fc1b62214fa51210555a0e1c2feb
Mikkel Løkke <ml@acto.dk>
2020-01-28 16:22:46 +0100

    Added Hazelcast username/password configuration

--- auth/src/main/java/dk/acto/auth/TokenFactory.java
+++ auth/src/main/java/dk/acto/auth/TokenFactory.java
@@ -6,26 +6,29 @@ import com.google.common.io.BaseEncoding;
 import io.vavr.control.Try;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.Cipher;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.SecureRandom;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.ZonedDateTime;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
 public class TokenFactory {
-	
+
 	private final KeyPair keys;
-	
+
 	public TokenFactory() {
 		this.keys = Try.of(() -> KeyPairGenerator.getInstance("RSA"))
 				.andThen(x -> x.initialize(1024, new SecureRandom()))
 				.map(KeyPairGenerator::generateKeyPair)
 				.get();
 	}
-	
+
 	public String generateToken(String subject, String idp, String name) {
 		return Try.of(() -> Algorithm.RSA512((RSAPublicKey) keys.getPublic(), (RSAPrivateKey) keys.getPrivate()))
 				.map(x -> JWT.create()
@@ -62,10 +65,18 @@ public class TokenFactory {
 						.sign(x))
 				.get();
 	}
-	
+
 	public String getPublicKey() {
 		return BaseEncoding.base64().omitPadding().encode(
 				keys.getPublic().getEncoded()
 		);
+	}
+
+	public String decryptString(String encrypted) {
+		return Try.of(() -> Cipher.getInstance("RSA/ECB/PKCS1Padding"))
+				.andThenTry(x -> x.init(Cipher.DECRYPT_MODE, keys.getPrivate()))
+				.mapTry(x -> x.doFinal(Base64.getDecoder().decode(encrypted)))
+				.map(x -> new String(x, StandardCharsets.UTF_8))
+				.getOrNull();
 	}
 }
---
+++ auth/src/main/java/dk/acto/auth/providers/HazelcastProvider.java
@@ -0,0 +1,48 @@
+package dk.acto.auth.providers;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import dk.acto.auth.ActoConf;
+import dk.acto.auth.TokenFactory;
+import dk.acto.auth.services.ServiceHelper;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class HazelcastProvider implements Provider {
+    private final TokenFactory tokenFactory;
+    private final ActoConf actoConf;
+    private final HazelcastInstance hazelcastInstance;
+
+    public HazelcastProvider(TokenFactory tokenFactory,
+                             ActoConf actoConf,
+                             @Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
+        this.tokenFactory = tokenFactory;
+        this.actoConf = actoConf;
+        this.hazelcastInstance = hazelcastInstance;
+    }
+
+    @Override
+    public String authenticate() {
+            return "/hazelcast/login";
+    }
+
+    public String callback(final String email, final String password) {
+        IMap<String, String> map = hazelcastInstance.getMap("fafnir-user");
+        return Optional.ofNullable(map.get(email))
+                .map(this::decryptPassword)
+                .filter(password::equals)
+                .map(x -> tokenFactory.generateToken(
+                        email,
+                        "hazelcast",
+                        email))
+                .map(x -> ServiceHelper.getJwtUrl(actoConf, x))
+                .orElse(actoConf.getFailureUrl());
+    }
+
+    private String decryptPassword(String encryptedPassword) {
+        return tokenFactory.decryptString(encryptedPassword);
+    }
+}
