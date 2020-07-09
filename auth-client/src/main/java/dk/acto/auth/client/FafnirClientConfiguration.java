64a6068eafa34eda55a26319690ef3185b21643b
Elias Jørgensen <ej@acto.dk>
2020-07-09 09:31:16 +0200

    Add configuration class to client

---
+++ auth-client/src/main/java/dk/acto/auth/client/FafnirClientConfiguration.java
@@ -0,0 +1,8 @@
+package dk.acto.auth.client;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class FafnirClientConfiguration {}
