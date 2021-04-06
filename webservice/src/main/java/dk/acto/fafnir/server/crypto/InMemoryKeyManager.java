ea5a4fca86e4bd8b0ad2d799fc7ebe0cb9b8d58c
Mikkel <m.loekke@m2p.dk>
2021-04-06 19:24:25 +0200

    EOD commit.

---
+++ webservice/src/main/java/dk/acto/fafnir/server/crypto/InMemoryKeyManager.java
@@ -0,0 +1,26 @@
+package dk.acto.fafnir.server.crypto;
+
+import io.vavr.control.Try;
+import lombok.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@Value
+@Component
+public class InMemoryKeyManager implements RsaKeyManager{
+    RSAPublicKey publicKey;
+    RSAPrivateKey privateKey;
+
+    public InMemoryKeyManager() {
+        var keys = Try.of(() -> KeyPairGenerator.getInstance("RSA"))
+                .andThen(x -> x.initialize(1024, new SecureRandom()))
+                .map(KeyPairGenerator::generateKeyPair)
+                .get();
+        publicKey = (RSAPublicKey) keys.getPublic();
+        privateKey = (RSAPrivateKey) keys.getPrivate();
+    }
+}
