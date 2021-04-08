cb8d1384298feb74aa3e5a01ce8eece12f55f098
Mikkel <m.loekke@m2p.dk>
2021-04-08 10:54:28 +0200

    Possible fix for missing BC provider for tests.

--- webservice/src/test/java/dk/acto/fafnir/server/crypto/KeyStoreKeyManagerTest.java
+++ webservice/src/test/java/dk/acto/fafnir/server/crypto/KeyStoreKeyManagerTest.java
@@ -1,12 +1,16 @@
 package dk.acto.fafnir.server.crypto;
 
 import dk.acto.fafnir.client.FafnirClientConfiguration;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.security.Security;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +20,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class KeyStoreKeyManagerTest {
     @Autowired
     private RsaKeyManager rsaKeyManager;
+
+    @BeforeAll
+    static void beforeAll() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     @Test
     void isRightBean() {
