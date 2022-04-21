d89a59af704fc6545523dc28808006eaab47675b
Aksel Nordvig Ladegaard <an@acto.dk>
2022-04-21 15:23:47 +0200

    merge

--- ui/config/webpack.config.js
+++ ui/config/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 //  To do
 //  1. load styles - possibly compile sass with certain parameters
 // 1.1 e.g., autoprefixing and minification
-// 2. generate the templates? 
+// 2. generate the templates?
 
 
 module.exports = {
