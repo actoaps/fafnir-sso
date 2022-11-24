049279a75434c002d057c94a32d87c021e2a22db
Omar A. Hachach <contact@omarhachach.com>
2022-11-24 14:02:36 +0100

    Use JKS to add x509 certificate to add keys to saml

--- sso/src/main/java/dk/acto/fafnir/sso/saml/UpdateableRelyingPartyRegistrationRepository.java
+++ sso/src/main/java/dk/acto/fafnir/sso/saml/UpdateableRelyingPartyRegistrationRepository.java
@@ -1,5 +1,9 @@
 package dk.acto.fafnir.sso.saml;
 
+import dk.acto.fafnir.api.crypto.RsaKeyManager;
+import dk.acto.fafnir.api.crypto.X509CertificateManager;
+import lombok.AllArgsConstructor;
+import org.springframework.security.saml2.core.Saml2X509Credential;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrations;
@@ -11,13 +15,19 @@ import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
+@AllArgsConstructor
 public class UpdateableRelyingPartyRegistrationRepository
         implements RelyingPartyRegistrationRepository, Iterable<RelyingPartyRegistration> {
+
+    private final RsaKeyManager rsaKeyManager;
+    private final X509CertificateManager certificateManager;
     private final Map<String, RelyingPartyRegistration> relyingPartyProperties = new ConcurrentHashMap<>();
 
     public RelyingPartyRegistration addRelyingPartyRegistration(String registrationId, String metadataLocation) {
         var relyingParty = RelyingPartyRegistrations.fromMetadataLocation(metadataLocation)
                 .registrationId(registrationId)
+                .signingX509Credentials(x -> x.add(Saml2X509Credential.signing(rsaKeyManager.getPrivateKey(), certificateManager.getCertificate())))
+                .decryptionX509Credentials(x -> x.add(Saml2X509Credential.decryption(rsaKeyManager.getPrivateKey(), certificateManager.getCertificate())))
                 .build();
 
         relyingPartyProperties.put(registrationId, relyingParty);
