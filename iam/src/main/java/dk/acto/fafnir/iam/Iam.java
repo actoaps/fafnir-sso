8a20b0ec16d528738a2a9c2a811571f879853201
Omar A. Hachach <contact@omarhachach.com>
2022-12-28 11:39:37 +0100

    Add FafnirClientConfiguration for autoconfiguration of Fafnir

--- iam/src/main/java/dk/acto/fafnir/iam/Iam.java
+++ iam/src/main/java/dk/acto/fafnir/iam/Iam.java
@@ -1,14 +1,17 @@
 package dk.acto.fafnir.iam;
 
 import dk.acto.fafnir.api.provider.metadata.MetadataProvider;
+import dk.acto.fafnir.client.FafnirClientConfiguration;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 import java.security.Security;
 
 @SpringBootApplication
+@Import(FafnirClientConfiguration.class)
 @Profile("!test")
 public class Iam {
 
