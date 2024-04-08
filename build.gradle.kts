3494add306e1fa74e0c387ace88b57ee4b10f15b
Jonas Hein <jh@acto.dk>
2024-04-08 16:29:49 +0200

    added maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,27 @@
 plugins {
     jacoco
+    `java-library`
+    `maven-publish`
     id("org.sonarqube") version "3.5.0.2730"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0-rc-2"
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/actoaps/fafnir-sso")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: "fallbackUsername"
+                password = System.getenv("GITHUB_TOKEN") ?: "fallbackPassword"
+            }
+        }
+    }
 }
 
 jacoco {
